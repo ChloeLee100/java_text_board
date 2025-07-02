fa30a811107f8765230e95f4db6ab3792e7ea05d
Chloe Lee <yeseo4615@gmail.com>
2025-07-02 16:26:03 +1000

    exit function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,25 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("== Java Text Board Start ==");
 
+        while (true) {
+            System.out.print("Order) ");
+            String cmd = sc.nextLine();
+
+            if (cmd.equals("exit")) {
+                System.out.println("Program Ends");
+                break;
+            }
+
+            System.out.printf("Given Order : %s\n", cmd);
+        }
+
         System.out.println("== Java Text Board End ==");
+        sc.close();
     }
 }
