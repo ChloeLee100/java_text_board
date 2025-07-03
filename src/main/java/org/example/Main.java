7ce22c57238c80dceb90ba9adf278b16cb50f4f8
Chloe Lee <yeseo4615@gmail.com>
2025-07-03 17:15:12 +1000

    article class created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,21 +5,52 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int lastArticleId = 0;
         System.out.println("== Java Text Board Start ==");
 
         while (true) {
             System.out.print("Order) ");
             String cmd = sc.nextLine();
 
-            if (cmd.equals("exit")) {
+            if (cmd.equals("/usr/article/write")) {
+                System.out.println("== Aricle Writing ==");
+                System.out.print("Title : ");
+                String subject = sc.nextLine();
+
+                System.out.print("Content : ");
+                String content = sc.nextLine();
+
+                int id = ++lastArticleId;
+
+                Article article = new Article();
+                article.id = id;
+                article.subject = subject;
+                article.content = content;
+
+                System.out.println("Created Article : " + article);
+                System.out.printf("Aricle %d created.\n", article.id);
+            }
+            else if (cmd.equals("exit")) {
                 System.out.println("Program Ends");
                 break;
             }
-
-            System.out.printf("Given Order : %s\n", cmd);
+            else {
+                System.out.println("Incorrect Order");
+            }
         }
 
         System.out.println("== Java Text Board End ==");
         sc.close();
+    }
+}
+
+class Article {
+    int id;
+    String subject;
+    String content;
+
+    @Override
+    public String toString() {
+        return "{id: %d, subject: \"%s\", content: \"%s\"}".formatted(id, subject, content);
     }
 }
